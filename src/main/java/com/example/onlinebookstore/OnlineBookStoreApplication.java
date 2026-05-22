c3bc333563c78f3c279ba5a4d02d58919377a570
hero <antonstendal19@gmail.com>
2026-05-22 13:48:18 +0200

    Add CI, checkstyle and test H2 config

--- src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
@@ -1,4 +1,4 @@
-package com.example.online_book_store;
+package com.example.onlinebookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnlineBookStoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineBookStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookStoreApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/onlinebookstore/OnlineBookStoreApplicationTests.java
+++ src/test/java/com/example/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.online_book_store;
+package com.example.onlinebookstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
